042e274a2f14d8104f2ca8906129fb9069520bb2
m-uemura <ju88991@gmail.com>
2022-05-03 05:05:59 +0000

    Finish ch10

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -15,6 +15,10 @@ module SessionsHelper
     end
   end
   
+  def current_user?(user)
+    user && user == current_user
+  end
+  
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
@@ -36,4 +40,14 @@ module SessionsHelper
     session.delete(:user_id)  
     @current_user = nil
   end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+  
 end
