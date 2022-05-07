c5ac4830b177167f68a5598b38ceaa8030977534
m-uemura <ju88991@gmail.com>
2022-05-07 07:19:23 +0000

    Finish ch12

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
+  
   validates :name,  presence: true, length: { maximum: 50 }
   validates :password, presence: true, length: { minimum: 6}, allow_nil: true
   
@@ -10,6 +11,7 @@ class User < ApplicationRecord
               length: { maximum: 50 }, 
               format: { with: VALID_EMAIL_REGEX }, 
               uniqueness: true
+              
   has_secure_password
   
   def User.digest(string)
@@ -46,13 +48,26 @@ class User < ApplicationRecord
     UserMailer.account_activation(self).deliver_now
   end
   
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end 
+  
+  # パスワード再設定の期限が切れている場合はtrueを返す
+  def password_reset_expired?
+    self.reset_sent_at < 2.hours.ago
+  end
+  
   private 
-    # メールアドレスをすべて小文字にする
     def downcase_email
       self.email = email.downcase
     end
 
-    # 有効化トークンとダイジェストを作成および代入する
     def create_activation_digest
       self.activation_token  = User.new_token
       self.activation_digest = User.digest(activation_token)
