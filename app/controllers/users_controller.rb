e84e53c1d08557458959ab7bef7aca0c63e115a4
m-uemura <ju88991@gmail.com>
2022-05-08 13:39:02 +0900

    ch13, 1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_uesr, only: [:index, :edit, :update, :destroy]
-  before_action :correct_uesr,   only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
   
   
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def new
@@ -53,23 +54,21 @@ class UsersController < ApplicationController
   end
   
   private
-
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
-  end
-  
-  def logged_in_uesr
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in"
-      redirect_to login_url
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
     end
-  end
-  
-  def correct_uesr
-    @user = User.find(params[:id])
-    redirect_to(root_url) unless current_user? @user
-  end
-  
+
+    # beforeフィルター
+
+    # 正しいユーザーかどうかを確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    # 管理者かどうかを確認
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
